6f5c2aac9dfd2b3d241c3df313f6a13f41c16461
Weiqing Xu <weiqxu@apache.org>
2026-04-27 19:35:47 +0800

    build: remove SQL Java and HDFS globals in local-lite

--- core/sql/nskgmake/Makerules.mk
+++ core/sql/nskgmake/Makerules.mk
@@ -291,7 +291,11 @@ mavenbuild_hdp:
 	cp -pf ../target/trafodion-sql-hdp*.jar $(TRAF_HOME)/export/lib
 
 # This is where the top-level is declared to build everything.
-buildall: $(FINAL_LIBS) $(FINAL_DLLS) $(FINAL_INSTALL_OBJS) $(FINAL_EXES) mavenbuild mavenbuild_hdp mavenbuild_apache 
+ifeq ($(TRAF_LOCAL_LITE),1)
+buildall: $(FINAL_LIBS) $(FINAL_DLLS) $(FINAL_INSTALL_OBJS) $(FINAL_EXES)
+else
+buildall: $(FINAL_LIBS) $(FINAL_DLLS) $(FINAL_INSTALL_OBJS) $(FINAL_EXES) mavenbuild mavenbuild_hdp mavenbuild_apache
+endif
 
 clean:
 	@echo "Removing intermediate objects for $(TARGTYPE)/$(ARCHBITS)/$(FLAVOR)"
